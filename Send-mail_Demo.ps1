101ce37e15bc7244cf9c75178a7e865162cdf49b
Harry C <coolsekali@hotmail.com>
2020-10-27 11:24:13 -0700

    Add files via upload

---
+++ Send-mail_Demo.ps1
@@ -0,0 +1,16 @@
+﻿$emailcred = ""
+$smtp = ""
+
+$failed = Get-SqlAgentJobHistory -ServerInstance sql2019-01 -Since Midnight -OutcomesType Failed | where StepName -ne "(Job outcome)" 
+
+
+if($failed){
+    $Subject = $failed.JobName + " has failed!"
+    $Body = $failed.JobName + " failed at " + $failed.RunDate + " with the following error message: "  
+    
+} else {
+    $Subject = "No Jobs Failure Found!" 
+    $Body  = "No failed jobs!"
+}
+
+Send-MailMessage -To $To -From $From -Subject $Subject -Body $Body -SmtpServer $smtp -Credential $emailcred
